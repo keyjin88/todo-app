93c15e555c0ed1617a2788e528fe1df2d8eb8a22
av <keyjin88@gmail.com>
2023-04-15 11:30:01 +0300

    Реализовал работу с конфигами и подключил миграции БД через утилиту golang-migrate

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"github.com/keyjin88/todo-app/pkg/handler"
 	"github.com/keyjin88/todo-app/pkg/repository"
 	"github.com/keyjin88/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
 	repos := repository.New()
 	services := service.New(repos)
 	handlers := handler.New(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoute()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoute()); err != nil {
 		log.Fatalf("error occurred while server running: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,7 @@
+drop table lists_items;
+drop table todo_items;
+drop table users;
+drop table users_lists;
+drop table todo_lists;
+
+
